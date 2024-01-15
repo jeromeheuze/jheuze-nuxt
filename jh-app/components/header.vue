a8de8cb58e361063529986decc53369dc47e440b
Spectrum3900 <jeromeheuze@gmail.com>
2024-01-15 14:40:14 -0800

    Update header.vue

--- jh-app/components/header.vue
+++ jh-app/components/header.vue
@@ -5,11 +5,14 @@
         <h1 class="mx-auto my-0 text-uppercase">Jerome Heuze</h1>
         <h2 class="text-white mx-auto mt-2 mb-5">Indie game developer, Blender3D enthusiast, Front End Developer, Outdoor man!</h2>
         <div class="ctas">
-          <a href="https://www.behance.net/jerome-heuze" class="btn btn-secondary">Portfolio</a>
+          <a href="https://www.behance.net/jerome-heuze" target="_blank" class="btn btn-secondary">Portfolio</a>
+          <a href="https://www.blurb.com/user/jeromeheuze" target="_blank" class="btn btn-primary">Books</a>
           <nuxt-link to="/blog/" class="btn btn-secondary">Blog</nuxt-link>
-          <a href="https://www.linkedin.com/in/jeromeheuze/" rel="noreferrer" class="btn btn-primary">Resume</a>
+          <a href="https://www.linkedin.com/in/jeromeheuze/" target="_blank" rel="noreferrer" class="btn btn-primary">Resume</a>
         </div>
       </div>
     </div>
   </header>
 </template>
+<script setup>
+</script>
