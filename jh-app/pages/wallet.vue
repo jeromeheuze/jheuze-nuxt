7c6bc3e3056fbf9decf863e64be8d012a4b36156
Spectrum3900 <jeromeheuze@gmail.com>
2024-03-31 20:35:31 -0700

    Create wallet.vue

---
+++ jh-app/pages/wallet.vue
@@ -0,0 +1,49 @@
+<template>
+  <LazyHydrate when-visible>
+
+    <main>
+      <Header/>
+      <section class="about-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-2">
+              <h2 class="text-white mb-4 text-center">Wallet</h2>
+              <p class="text-white-50">YTN: YcrrohT2pkqhPTSyaqFjKfoERZgMLyLn9w</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer/>
+    </main>
+
+  </LazyHydrate>
+</template>
+
+<script>
+import Header from "~/components/header";
+import Footer from "~/components/footer";
+import LazyHydrate from 'vue-lazy-hydration';
+export default {
+  head () {
+    return {
+      bodyAttrs: {
+        class: 'v1'
+      },
+      title: 'Wallet',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Indie game developer, Front End Developer, Crypto mining and trading enthusiast!'
+        }
+      ],
+      style: [
+      ],
+      script: [
+      ]
+    }
+  },
+  modules: [],
+  components: {LazyHydrate, Footer, Header}
+}
+</script>
