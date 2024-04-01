ad67a50f785b8182fba32df9a49b3a4ef73297a5
Spectrum3900 <jeromeheuze@gmail.com>
2024-03-31 20:42:49 -0700

    Update wallet.vue

--- jh-app/pages/wallet.vue
+++ jh-app/pages/wallet.vue
@@ -9,6 +9,7 @@
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-2">
               <h2 class="text-white mb-4 text-center">Wallet</h2>
               <p class="text-white-50">YTN: YcrrohT2pkqhPTSyaqFjKfoERZgMLyLn9w</p>
+              <p class="text-white-50">./minerd -a yespowerR16 -o stratum+tcp://cpu-pool.com:63368 -u YcrrohT2pkqhPTSyaqFjKfoERZgMLyLn9w -p x -t 4</p>
             </div>
           </div>
         </div>
