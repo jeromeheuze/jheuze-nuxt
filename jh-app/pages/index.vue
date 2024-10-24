5e22e972dc1e24cc07181ba1dc7671c80deae0ce
Spectrum3900 <jeromeheuze@gmail.com>
2024-10-24 10:04:51 -0700

    Update index.vue

--- jh-app/pages/index.vue
+++ jh-app/pages/index.vue
@@ -88,11 +88,11 @@
                     <h4 class="text-white">Other Websites I own and maintain:</h4>
                     <ul style="margin-top: 1rem;">
                       <li><a target="_blank" href="https://10kgame.studio">10kgame.studio</a> PHP - HTML/CSS - Unity + Defold</li>
-                      <li><a target="_blank" href="https://academialore.com">academialore.com</a> PHP - HTML/CSS</li>
+                      <li><a target="_blank" href="https://academialore.com">academialore.com</a> TBD -- Defold 3D Game</li>
                       <li><a target="_blank" href="https://e2.university">e2.university</a> PHP - HTML/CSS</li>
                       <li><a target="_blank" href="https://forum.e2.university">Forum e2.university</a> PHP - HTML/CSS/Laravel</li>
                       <li><a target="_blank" href="https://e2japan.com">e2japan.com</a> PHP - HTML/CSS/MySQLi/MapBox Integration</li>
-                      <li><a target="_blank" href="https://e2production.com">e2production.com</a> NuxtJS 2.x / Netlify</li>
+                      <li><a target="_blank" href="https://e2production.com">e2production.com</a> PHP - HTML/CSS</li>
                       <li><a target="_blank" href="https://entropia.estate">entropia.estate</a> NuxtJS 3.x / Netlify / Supabase</li>
                       <li><a target="_blank" href="https://entropiahub.com">entropiahub.com</a> PHP - HTML/CSS - MySQLi</li>
                       <li><a target="_blank" href="https://foma-asteroid.com">foma-asteroid.com</a> PHP - HTML/CSS</li>
@@ -100,7 +100,7 @@
                       <li><a target="_blank" href="https://gddmaker.com">gddmaker.com</a> PHP - HTML/CSS - MySQLi</li>
                       <li><a target="_blank" href="https://geolocationalmetaverse.com">geolocationalmetaverse.com</a> PHP - HTML/CSS</li>
                       <li><a target="_blank" href="https://heuzeproductions.com">heuzeproductions.com</a> PHP - HTML/CSS</li>
-                      <li><a target="_blank" href="https://kohibou.com">kohibou.com</a> TBD -- Defold Game</li>
+                      <li><a target="_blank" href="https://kohibou.com">kohibou.com</a> TBD -- Defold 2D Game</li>
                       <li><a target="_blank" href="https://openbooktutorials.com">openbooktutorials.com</a> NuxtJS 3.x / Netlify / Blog</li>
                       <li><a target="_blank" href="https://spacescience.degree">spacescience.degree</a> NuxtJS 3.x / Netlify</li>
                       <li><a target="_blank" href="https://sukarei-jewelry.com">sukarei-jewelry.com</a> TBD</li>
