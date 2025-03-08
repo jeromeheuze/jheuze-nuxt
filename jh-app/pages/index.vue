9e9a03185dd68768440a805765b532f2487b6b43
Spectrum3900 <jeromeheuze@gmail.com>
2025-03-08 10:59:52 -0800

    Update index.vue

--- jh-app/pages/index.vue
+++ jh-app/pages/index.vue
@@ -97,14 +97,16 @@
                       <li><a target="_blank" href="https://gddmaker.com">gddmaker.com</a> PHP - HTML/CSS - MySQLi</li>
                       <li><a target="_blank" href="https://heuzeproductions.com">heuzeproductions.com</a> PHP - HTML/CSS</li>
                       <li><a target="_blank" href="https://spacescience.degree">spacescience.degree</a> NuxtJS 3.x / Netlify</li>
+                      <li><a target="_blank" href="https://sukarei-jewelry.com">sukarei-jewelry.com</a> TBD</li>
                     </ul>
                     <h4 class="text-white">Websites: [Under DEVELOPMENT]</h4>
                     <ul style="margin-top: 1rem;">
-                      <li><a target="_blank" href="https://spaceshipadventures.com">spaceshipadventures.com</a> Space Game - Unreal / RPC / SBC</li>
-                      <li><a target="_blank" href="https://10kgame.studio">10kgame.studio</a> PHP - HTML/CSS - Unity / Defold</li>
+                      <li><a target="_blank" href="https://spaceshipadventures.com">spaceshipadventures.com</a> Space Game - Unity 6 / RPC / Raspberry Pi</li>
+                      <li><a target="_blank" href="https://10kgame.studio">10kgame.studio</a> PHP - HTML/CSS - Unity 6 + Godot 4.4 + Defold 1.97 - Itch.io</li>
                       <li><a target="_blank" href="https://academialore.com">academialore.com</a> Godot -- MMO LearningRPG</li>
                       <li><a target="_blank" href="https://kohibou.com">kohibou.com</a> Godot -- 2D Pixel Metaverse</li>
-                      <li><a target="_blank" href="https://sukarei-jewelry.com">sukarei-jewelry.com</a> TBD</li>
+                      <li><a target="_blank" href="https://videogameguide.ai">videogameguide.ai</a> TBD</li>
+                      <li><a target="_blank" href="https://IndieDev.games">IndieDev.games</a> TBD</li>
                     </ul>
                   </div>
                 </div>
